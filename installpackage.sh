b914b105979d4eacf9d8fa07f6a86df7af1bc9d0
CAHR92 <35469238+CAHR92@users.noreply.github.com>
2018-01-22 15:10:19 -0600

    Add files via upload

---
+++ installpackage.sh
@@ -0,0 +1,13 @@
+directorio="$(pwd)"
+sudo apt-get install python-pip --upgrade
+sudo apt-get install python3-pip --upgrade
+pip install --upgrade pip
+
+
+pip install $directorio/Paquetes/pyDes/. --upgrade
+pip install $directorio/Paquetes/base64/. --upgrade
+#pip install $directorio/Paquetes/urllib/. --upgrade
+#pip install $directorio/Paquetes/urllib.parse/. --upgrade
+pip install $directorio/Paquetes/re/. --upgrade
+pip install $directorio/Paquetes/random/. --upgrade
+pip install $directorio/Paquetes/numpy/. --upgrade
---
+++ uninstallpackage.sh
@@ -0,0 +1,6 @@
+pip uninstall pyDes
+pip uninstall b64img
+pip uninstall regex
+pip uninstall numpy
+#pip uninstall urllib
+#pip uninstall urllib.parse
